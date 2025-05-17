3bcca6bab64cea394bfbe7c65933ea0bbe7708df
hrsk <hrsk.yegor@gmail.com>
2025-05-17 17:09:12 +0300

    fix: add tasks reducer action creator

--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -3,9 +3,23 @@ import {
     createTodolistAC,
     removeTodolistAC,
 } from "./todolists-reducer.ts";
-import {createReducer} from "@reduxjs/toolkit";
+import {createAction, createReducer, nanoid} from "@reduxjs/toolkit";
 
 const initialState: TasksState = {}
+
+export const removeTaskActionCreator = createAction<{ todolistId: string, taskId: string }>('tasks/remove-task')
+export const createTaskActionCreator = createAction<{ todolistId: string, value: string }>('tasks/create-task')
+export const changeTaskStatusActionCreator = createAction<{
+    todolistId: string,
+    taskId: string,
+    isDone: boolean
+}>('tasks/change-task-status')
+export const changeTaskTitleActionCreator = createAction<{
+    todolistId: string,
+    taskId: string,
+    value: string
+}>('tasks/change-task-title')
+
 
 export const tasksReducer = createReducer(initialState, builder => {
     builder
@@ -15,31 +29,26 @@ export const tasksReducer = createReducer(initialState, builder => {
         .addCase(removeTodolistAC, (state, action) => {
             delete state[action.payload.todolistId]
         })
+        .addCase(removeTaskActionCreator, (state, action) => {
+            const task = state[action.payload.todolistId].findIndex(task => task.id === action.payload.taskId)
+
+            if (task !== -1) {
+                state[action.payload.todolistId].splice(task, 1)
+            }
+        })
+        .addCase(createTaskActionCreator, (state, action) => {
+            state[action.payload.todolistId].unshift({id: nanoid(), title: action.payload.value, isDone: false})
+        })
+        .addCase(changeTaskStatusActionCreator, (state, action) => {
+            const task = state[action.payload.todolistId].find(task => task.id === action.payload.taskId)
+            if (task) {
+                task.isDone = action.payload.isDone
+            }
+        })
+        .addCase(changeTaskTitleActionCreator, (state, action) => {
+            const task = state[action.payload.todolistId].find(task => task.id === action.payload.taskId)
+            if (task) {
+                task.title = action.payload.value
+            }
+        })
 })
-
-//action creators
-
-export const removeTaskActionCreator = (payload: { todolistId: string, taskId: string }) => {
-    return {
-        type: 'remove_task',
-        payload
-    } as const
-}
-export const createTaskActionCreator = (payload: { todolistId: string, value: string }) => {
-    return {
-        type: 'create_task',
-        payload
-    } as const
-}
-export const changeTaskStatusActionCreator = (payload: { todolistId: string, taskId: string, isDone: boolean }) => {
-    return {
-        type: 'change_task_status',
-        payload
-    } as const
-}
-export const changeTaskTitleActionCreator = (payload: { todolistId: string, taskId: string, value: string }) => {
-    return {
-        type: 'change_task_title',
-        payload
-    } as const
-}
